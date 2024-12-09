783b69a6a0de156a2a596a03642fb14f49872b56
Linus045 <lwillms88@gmail.com>
2024-12-09 17:25:48 +0100

    chore: Updates keymap

--- keyboards/splitkb/aurora/sofle_v2/rev1/keymaps/Linus045/keymap_extras.c
+++ keyboards/splitkb/aurora/sofle_v2/rev1/keymaps/Linus045/keymap_extras.c
@@ -8,3 +8,26 @@ void keyboard_pre_init_user(void) {
   gpio_set_pin_output(24);
   gpio_write_pin_high(24);
 }
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   // Handle Ctrl LED state
+//   switch (keycode) {
+//   case KC_SPC:
+//     if (record->event.pressed) {
+//       gpio_write_pin_low(24);
+//     } else {
+//       gpio_write_pin_high(24);
+//     }
+//     break;
+//   }
+//   return true;
+// }
+
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//   if (layer_state_cmp(state, 2)) {
+//     gpio_write_pin_low(24);
+//   } else {
+//     gpio_write_pin_high(24);
+//   }
+//   return state;
+// }
